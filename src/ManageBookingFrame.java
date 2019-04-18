5082c00cfff42cc9192f707e216a6aa59582defb
Dell <amritaganguly111@gmail.com>
2019-04-18 23:08:51 +0600

    change commit

--- src/ManageBookingFrame.java
+++ src/ManageBookingFrame.java
@@ -12,9 +12,7 @@ import javax.swing.table.DefaultTableModel;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
