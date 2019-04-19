f2a013af7c0f0f8e00cb90011d91dbf666c2b78d
Aspire <Aspire@RaFsan>
2019-04-19 23:46:37 +0600

    first commit

--- src/db.java
+++ src/db.java
@@ -4,11 +4,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
